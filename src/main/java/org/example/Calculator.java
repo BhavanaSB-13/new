dce8933bae98d2d1ae7a490456421214d6b72776
BhavanaSB-13 <bhavanabhuvanesh.sb@gmail.com>
2024-11-05 12:43:51 +0530

    Initial commit with IntelliJ project

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.Scanner;
+    class Calculator{
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+        public int subtract(int a, int b) {
+            return a - b;
+        }
+
+        public int multiply(int a, int b) {
+            return a * b;
+        }
+
+        public int divide(int a, int b) {
+            if (b == 0) {
+                throw new ArithmeticException("Division by zero");
+            }
+            return a / b;
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter two numbers");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            Calculator calculator = new Calculator();
+            System.out.println("Addition: " + calculator.add(a, b));
+            System.out.println("Subtraction: " + calculator.subtract(a, b));
+            System.out.println("Multiplication: " + calculator.multiply(a, b));
+            System.out.println("Division: " + calculator.divide(a, b));
+
+            sc.close();
+        }
+    }
---
+++ src/test/java/org/example/CalciTest.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalciTest {
+    @Test
+    public void testAddition() {
+        Calculator add = new Calculator();
+        assertEquals(5, add.add(2, 3));
+        assertEquals(-1, add.add(-2, 1));
+        assertEquals(0, add.add(0, 0));
+        assertEquals(100, add.add(50, 50));
+    }
+
+    @Test
+    public void testSubtraction() {
+        Calculator add = new Calculator();
+        assertEquals(-1, add.subtract(2, 3));
+        assertEquals(-3, add.subtract(-2, 1));
+        assertEquals(0, add.subtract(0, 0));
+        assertEquals(0, add.subtract(50, 50));
+    }
+
+    @Test
+    public void testMultiplication() {
+        Calculator add = new Calculator();
+        assertEquals(6, add.multiply(2, 3));
+        assertEquals(-2, add.multiply(-2, 1));
+        assertEquals(0, add.multiply(0, 10));
+        assertEquals(2500, add.multiply(50, 50));
+    }
+
+    @Test
+    public void testDivision() {
+        Calculator add = new Calculator();
+        assertEquals(2, add.divide(6, 3));
+        assertEquals(-2, add.divide(-4, 2));
+        assertEquals(0, add.divide(0, 1));
+        assertEquals(1, add.divide(50, 50));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivisionByZero() {
+        Calculator add = new Calculator();
+        add.divide(5, 0);
+    }
+}
